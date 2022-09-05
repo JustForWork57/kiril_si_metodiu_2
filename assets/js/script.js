98d27932161d566adeca1e62040ca4ce964aede9
Tudor <63875270+SexomQ@users.noreply.github.com>
2022-09-05 17:46:01 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+// ========index.html=========
+
+// ScrollButton
+function scrollFunction() {
+  document.body.scrollTop = 550; // For Safari
+  document.documentElement.scrollTop = 550; // For Chrome, Firefox, IE and Opera
+}
+
+
